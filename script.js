c48ad7133ecd30dd4ac8077d01716191a84e025a
zvika-ganz <zvika.ganz@gmail.com>
2025-08-30 22:22:59 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('copyEmail').addEventListener('click', function() {
-  navigator.clipboard.writeText('zvika.ganz@gmail.com');
-  this.textContent = 'Copied!';
-  setTimeout(()=>{ this.textContent = 'Copy Email'; },1500);
-});
